3c13a277693e35d804a4c396187f1c092545eb35
Mark Manley <mark.manley3@icloud.com>
2023-06-09 00:03:54 -0400

    algorithm: rotate NxN matrix with XOR swap

---
+++ src/algo/rotate_NxN_matrix.ts
@@ -0,0 +1,13 @@
+export function rotate_NxN_matrix<T>(a: T[][]) {
+  a.reverse();
+  for (let i = 0; i < a.length; i++) {
+    for (let j = 0; j < i; j++) {
+      // Switch a[i][j] and a[j][i]
+      // With XOR swap
+      a[i][j] ^= a[j][i];
+      a[j][i] ^= a[i][j];
+      a[i][j] ^= a[j][i];
+    }
+  }
+  return a;
+}
